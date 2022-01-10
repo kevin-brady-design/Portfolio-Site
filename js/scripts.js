71726de0eae7ac9048a971d019d8f6b059b8998a
kevin-brady-design <info@kevinbradyphotography.com>
2022-01-10 13:48:08 -0500

    Adds JSQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log ("Welcome to Kevin's portfolio.")
+
+function menuToggle () {
+  var x = document.getElementById ('myNavtoggle');
+  x.className += ' responsive';
+} else {
+ x.className = 'navtoggle';
+}
+}
