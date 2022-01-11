71962820b0b98995dbdb6348ed3ceb2cdf2afab7
kevin-brady-design <info@kevinbradyphotography.com>
2022-01-11 12:37:38 -0500

    Updates function menuToggle js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-console.log ("Welcome to Kevin's portfolio.")
+console.log("Welcome to Kevin's portfolio.")
 
-function menuToggle () {
-  var x = document.getElementById ('myNavtoggle');
-  x.className += ' responsive';
+function menuToggle() {
+var x = document.getElementById('myNavtoggle');
+if (x.className === 'navtoggle') {
+x.className += ' responsive';
 } else {
- x.className = 'navtoggle';
+x.className = 'navtoggle';
 }
 }
